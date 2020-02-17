20a92e56997c6a1a96897cfc5e60ae410bedbd20
Павел <pavelpolkovnikov334@gmail.com>
2020-02-18 00:49:43 +0700

    key handling up/down/right/left

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 SCREEN_SIZE = [920, 520]
@@ -18,6 +19,15 @@ class Example(QMainWindow):
         uic.loadUi('main_menu.ui', self)
         self.getImage(START_COORDS, START_SPN)
         self.update_map()
+        self.coords = START_COORDS
+
+    def move_Image(self, changing_coords):
+        self.coords[0] += changing_coords[0]
+        self.coords[1] += changing_coords[1]
+        self.getImage(self.coords, START_SPN)
+        self.image.close()
+        self.update_map()
+        self.image.show()
 
     def getImage(self, coords, spn, style="map"):
         params = {"ll": ",".join(map(str, coords)),
@@ -40,6 +50,16 @@ class Example(QMainWindow):
         self.image.move(280, 20)
         self.image.resize(630, 470)
         self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Right:
+            self.move_Image([START_SPN[0] * 2, 0])
+        if e.key() == Qt.Key_Left:
+            self.move_Image([START_SPN[0] * -2, 0])
+        if e.key() == Qt.Key_Up:
+            self.move_Image([0, START_SPN[1] * 2])
+        if e.key() == Qt.Key_Down:
+            self.move_Image([0, START_SPN[1] * -2])
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
